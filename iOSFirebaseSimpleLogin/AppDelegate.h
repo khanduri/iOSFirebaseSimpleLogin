b283f9d273f303a18da8d322150ef964cf461278
Prashant Khanduri <pkhanduri@jawbone.com>
2014-06-01 13:39:21 -0700

    Initial Commit

---
+++ iOSFirebaseSimpleLogin/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSFirebaseSimpleLogin
+//
+//  Created by Prashant Khanduri on 6/1/14.
+//  Copyright (c) 2014 KDeal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
